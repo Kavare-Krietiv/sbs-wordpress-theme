99d30a452caa7539cf31f2b779100c0ef05572ae
ngangavic <ngangavictor10@gmail.com>
2020-07-29 16:59:22 +0300

    footer

---
+++ footer.php
@@ -0,0 +1,148 @@
+<footer class="navbar sticky-bottom footer">
+    <div class="container" style="padding-bottom: 50px">
+        <div class="footer-top">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+                        <img src="<?php echo get_template_directory_uri();?>/assests/SBS_Logo_White.png" class="img-fluid" width="100px"
+                             style="padding-bottom: 10px">
+                        <p class="footer-text">WE ARE SHUNGWAYA BROADCASTING SERVICES ABREVIATED TO
+                            MEDIA HOUSE LICENSED TO SERVE THROUGHOUT THE “ SBS ”
+                            COAST WHICH IS A REGIONAL REGION AND NEWS BEGAN June 16, 2020
+                            BROADCASTING ON FREQUENCY 94.4 FM KILIFI ON THE 27 TH AUGUST 2018 FROM ITS
+                            TRANSMITTERS IN MAMBRUI AND JARIBUNI COVERING ALL THE SIX COASTAL COUNTIES : KILIFI
+                            ,
+                            MOMBASA , KWALE , TAITA-TAVETA , LAMU AND TANA RIVER .</p>
+                        <p class="footer-text">OUR MISSION HAS REMAINED THE SAME FOR ALMOST 3 YEARS , ACTING IN
+                            THE
+                            PUBLIC
+                            INTEREST
+                            ,
+                            SERVING ALL AUDIENCES THROUGH THE PROVISION OF IMPARTIAL , HIGH - QUALITY AND
+                            DISTINCTIVE OUTPUT AND SERVICES WHICH INFORM , EDUCATE AND ENTERTAIN .</p>
+
+                        <div class="footer-communication">
+                            <div class="row">
+                                <img src="<?php echo get_template_directory_uri();?>/assests/Location_Icon_White.png" width="30px" height="20px">
+                                <p>OFF KILIFI MALINDI HIGHWAY . BLOCK 1,<br/>
+                                    GREEN ESTATE . PO BOX 1013-80108 KILIFI</p>
+                            </div>
+
+                            <div class="row">
+                                <img src="<?php echo get_template_directory_uri();?>/assests/Phone_Icon_White.png" width="30px" height="20px">
+                                <p>+254 724 716 386 . +254 786 716 386
+                                    +254 724 173 370</p>
+                            </div>
+
+                            <div class="row">
+                                <img src="<?php echo get_template_directory_uri();?>/assests/Email_Website_Icon_White.png" width="30px" height="20px">
+                                <p>SBSRADIOSTATION@GMAIL.COM<br/>
+                                    INFO@SBSRADIO.LIVE . LISTEN@SBSRADIO.LIVE</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+
+                        <h3 style="color: #ffffff;font-weight: bold">MAILING LIST</h3>
+                        <p style="color: #E3E3E2;font-size: 9px;text-align: center">If you would like to be on
+                            our
+                            mailing list and
+                            receive our latest news and info, please fill in your
+                            Email address in the tub below you are in.</p>
+                        <form>
+                            <div class="input-group input-group-sm">
+                                <input type="text" class="form-control" placeholder="Your Email Address"
+                                       required>
+                                <div class="input-group-append">
+                                    <button class="btn btn-info"><i class="fas fa-caret-down"></i></button>
+                                </div>
+                            </div>
+                        </form>
+                        <p style="color: #E3E3E2;font-size: 9px">We respect your privacy</p>
+
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                        <br/>
+                        <p style="color: white;font-weight: bold">LATEST NEWS</p>
+                        <div class="row">
+                            <img src="<?php echo get_template_directory_uri();?>/assests/SBS_Radio_News_Red.png" width="100px" height="50px">
+                            <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+                                <p style="font-size: 10px;color: #E3E3E2">Wazazi waliopoteza wanafunzi kwenye
+                                    mkanganyo
+                                    shule ya kakamega, wataka ripoti kuhusu mkasa
+                                    huo kutolewa<br/><b>June 16,2020</b></p>
+                            </div>
+                        </div>
+                        <hr style="border: 1px solid #E3E3E2;"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<div class="footer-bottom">
+    <div class="container">
+        <div class="float-left" style="font-weight: bold">COPYRIGHT © <span style="color: #E41321">SBS</span>
+            <span
+                style="color: #0553A0">RADIO 94.4 FM</span> 2020
+        </div>
+        <div class="float-right" style="font-weight: bold;padding-bottom: 30px">DESIGNED AT <a
+                href="http://kavarekrietiv.co.ke/" style="color: #E41321">KAVARE KRIETIV</a> HELP DESK
+            <span style="color: #E41321">020 7840388</span>
+        </div>
+    </div>
+</div>
+
+<!--alert modal-->
+<div class="modal fade" id="myModal">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">Alert</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <!-- Modal body -->
+            <div class="modal-body">
+                This site is under development.
+            </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<!--alert modal-->
+</div>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+        crossorigin="anonymous"></script>
+<script>
+    $(window).on('load', function () {
+        $('#spinner-wrapper').hide();
+        $(".main-content").css("display", "inline");
+
+    });
+</script>
+
+</body>
+</html>
