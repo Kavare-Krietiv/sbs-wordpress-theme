687ae6aa196a015e3a5948dcdceeeb12a55d3186
ngangavic <ngangavictor10@gmail.com>
2020-07-29 17:00:34 +0300

    single

---
+++ single.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<div class="container-fluid">
+    <h5><b><?php the_title(); ?></b></h5>
+
+    <?php if(has_post_thumbnail()):?>
+<img src="<?php the_post_thumbnail_url("smallest"); ?>" class="img-fluid">
+    <?php endif; ?>
+
+    <?php
+    if(have_posts()):while(have_posts()):the_post();
+        ?>
+
+        <?php the_content(); ?>
+
+    <?php endwhile; endif ?>
+
+</div>
+
+
+<?php get_footer(); ?>
